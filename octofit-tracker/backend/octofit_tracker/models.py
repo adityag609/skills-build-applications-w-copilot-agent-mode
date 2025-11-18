b856ab74fc4a10ec12956f00468ca07325eac1bb
adityag609 <adityag609@gmail.com>
2025-11-18 08:01:33 +0000

    test data fix

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,6 +1,8 @@
 from djongo import models
 
+
 class Team(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
     class Meta:
@@ -8,17 +10,21 @@ class Team(models.Model):
     def __str__(self):
         return self.name
 
+
 class User(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, related_name='members')
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, to_field='_id', related_name='members')
     class Meta:
         db_table = 'users'
     def __str__(self):
         return self.name
 
+
 class Activity(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, to_field='_id', related_name='activities')
     type = models.CharField(max_length=50)
     duration = models.IntegerField()  # minutes
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -27,7 +33,9 @@ class Activity(models.Model):
     def __str__(self):
         return f"{self.user.name} - {self.type}"
 
+
 class Workout(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     difficulty = models.CharField(max_length=50)
@@ -36,8 +44,10 @@ class Workout(models.Model):
     def __str__(self):
         return self.name
 
+
 class Leaderboard(models.Model):
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='leaderboard')
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, to_field='_id', related_name='leaderboard')
     points = models.IntegerField(default=0)
     class Meta:
         db_table = 'leaderboard'
