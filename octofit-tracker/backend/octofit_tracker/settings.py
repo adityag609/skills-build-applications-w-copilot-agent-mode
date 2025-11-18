f37bbe0e889e3825beb3ef3d2cbadf88e3ebbf10
adityag609 <adityag609@gmail.com>
2025-11-18 08:20:02 +0000

    Bug Fix

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -133,12 +133,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = ['*']
 CORS_ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
-ALLOWED_HOSTS = ['*']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
